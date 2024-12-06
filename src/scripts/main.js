44ae3d681d3dd7b2075fdcdfe1dc97c95d732ed3
Vlad <vladyslav.vasylevych@gmail.com>
2024-12-06 09:45:15 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 // write your code here
+function main() {
+  const doc = document.documentElement;
+  const popul = [ ...doc.getElementsByClassName('population') ].map(elem => elem.innerHTML.split(',').join(''));
+
+  const total = popul.reduce((prev, elem) => +elem + prev, 0);
+  const average = total / popul.length;
+
+
+  const totalPopulEl = doc.getElementsByClassName('total-population')[0];
+  const avgPopulEl = doc.getElementsByClassName('average-population')[0];
+
+
+  totalPopulEl.innerText = numberWithCommas(total);
+  avgPopulEl.innerText = numberWithCommas(average);
+
+}
+
+main();
